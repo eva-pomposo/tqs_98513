4763fc5f4b1d89ab76e2431e21df0f02c1edb180
eva-pomposo <evabartolomeu@ua.pt>
2022-04-10 00:43:38 +0100

    fix lab5_1

---
+++ lab5/lab5_1/cucumber-skeleton/src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -0,0 +1,26 @@
+package io.cucumber.skeleton;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class StepDefinitions {
+    Belly belly = new Belly();
+
+    @Given("I have {int} cukes in my belly")
+    public void I_have_cukes_in_my_belly(int cukes) {
+        belly.eat(cukes);
+    }
+
+    @When("I wait {int} hour")
+    public void i_wait_hour(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        belly.waitHour(int1);
+        //assert...
+    }
+    @Then("my belly should growl")
+    public void my_belly_should_growl() {
+        // Write code here that turns the phrase above into concrete actions
+        belly.shouldGrowl();
+    }
+}
