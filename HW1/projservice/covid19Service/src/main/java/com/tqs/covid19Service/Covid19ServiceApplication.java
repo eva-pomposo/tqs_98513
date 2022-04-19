bc659daf50a1b21f6ef1cda02f6564c8d1f440e6
eva-pomposo <evabartolomeu@ua.pt>
2022-04-19 15:09:01 +0100

    create project maven in HW1

---
+++ HW1/projservice/covid19Service/src/main/java/com/tqs/covid19Service/Covid19ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tqs.covid19Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19ServiceApplication.class, args);
+	}
+
+}
---
+++ HW1/projservice/covid19Service/src/test/java/com/tqs/covid19Service/Covid19ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tqs.covid19Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
