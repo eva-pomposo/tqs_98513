8a04bcf741b6e839a4680e6a02d57c5d13f43c17
eva-pomposo <evabartolomeu@ua.pt>
2022-05-02 01:32:51 +0100

    try cucumber

--- HW1/projservice/covid19Service/src/main/java/com/tqs/covid19Service/model/Country.java
+++ HW1/projservice/covid19Service/src/main/java/com/tqs/covid19Service/model/Country.java
@@ -1,30 +1,11 @@
 package com.tqs.covid19Service.model;
 
-import lombok.Data;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.GenerationType;
-
-@Data
-@Entity
 public class Country {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     private String name;
 
-    public Country() {
-    }
-
     public Country(String name) {
         this.name = name;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public String getName() {
@@ -33,7 +14,32 @@ public class Country {
 
     public void setName(String name) {
         this.name = name;
-    }   
-    
+    }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Country other = (Country) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }   
+
+    
 }
